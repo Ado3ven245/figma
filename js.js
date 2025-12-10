d125a3b72b392fde2bad8a7a3820206707b4d807
Ado3ven245 <urahara2463663@gmail.com>
2025-12-10 16:55:32 +0100

    aaaaaaaaaa

--- js.js
+++ js.js
@@ -5,9 +5,40 @@ menu.addEventListener("click",function(event){
     let clickTarget = event.target;
     console.log(clickTarget);
     let activeBtn = document.querySelector(".active");
-    if(clickTarget.classList.contains(".menu")){
+    if(clickTarget.classList.contains("aa")){
         clickTarget.classList.add("active");
         activeBtn.classList.remove("active")
         
     }
 });
+
+let classLink = ".a1" ;
+
+window.onscroll = function(){
+   
+    let h =document.documentElement.clientHeight;
+      console.log(h)                
+
+    if(window.scrollY >= h*4 ){
+        classLink = ".a5";
+    }
+    else if(window.scrollY >= h*3){
+        classLink = ".a4";
+    }
+    else if(window.scrollY >= h*2){
+        classLink = ".a3";
+    }
+    else if(window.scrollY >= h){
+        classLink = ".a2";
+    }
+    else{
+        classLink = ".a1";
+    }
+    let activeBtn = this.document.querySelector(".active");
+    let newActiveBtn = document.querySelector(classLink);
+    if(!newActiveBtn.classList.contains("active")){
+        newActiveBtn.classList.add("active");
+        activeBtn.classList.remove("active")
+    };
+}
+
